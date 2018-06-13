c2e066e63cf2661fc1d12b760908ac9fb2788ba6
Robert J. Macomber <robertm@dilithium>
2018-06-12 22:08:50 -0700

    Fiddling with the examples more

--- examples/Data/Aeson/Streaming/Examples/JsonExplode.hs
+++ examples/Data/Aeson/Streaming/Examples/JsonExplode.hs
@@ -11,22 +11,24 @@ import Data.Conduit.Attoparsec (sinkParser)
 import Data.Aeson (Value)
 import Data.Aeson.Streaming
 import Data.ByteString (ByteString)
+import Data.DList (DList)
+import qualified Data.DList as DL
 
 type JsonExplode m a = ConduitT ByteString ([PathComponent], Value) m a
 
 -- Convert JSON input to a series of (path, atomic-value) pairs
 jsonExplode :: forall m. (MonadThrow m)
             => JsonExplode m ()
-jsonExplode = step pure [] =<< sinkParser root
+jsonExplode = step pure DL.empty =<< sinkParser root
   where
-    step :: (NextParser p -> JsonExplode m a) -> [PathComponent] -> ParseResult p -> JsonExplode m a
+    step :: (NextParser p -> JsonExplode m a) -> DList PathComponent -> ParseResult p -> JsonExplode m a
     step next path result =
       next =<< case result of
                  ArrayResult np -> loopCompound path np
                  ObjectResult np -> loopCompound path np
-                 AtomicResult np v -> np <$ yield (path, v)
-    loopCompound :: (PathableIndex c) => [PathComponent] -> NextParser ('In c p) -> JsonExplode m (NextParser p)
+                 AtomicResult np v -> np <$ yield (DL.toList path, v)
+    loopCompound :: (PathableIndex c) => DList PathComponent -> NextParser ('In c p) -> JsonExplode m (NextParser p)
     loopCompound l p =
       sinkParser p >>= \case
-        Element idx result -> step (loopCompound l) (pathComponent idx : l) result
+        Element idx result -> step (loopCompound l) (DL.snoc l (pathComponent idx)) result
         End np -> pure np
--- examples/Data/Aeson/Streaming/Examples/Util.hs
+++ examples/Data/Aeson/Streaming/Examples/Util.hs
@@ -15,17 +15,21 @@ import Data.Aeson (Value)
 import qualified Data.Aeson.Text as AT
 import Data.Aeson.Streaming
 import Text.Read (readMaybe)
+import Data.Char (isAsciiLower, isAsciiUpper, isDigit)
 
 tshow :: (Show a) => a -> Text
 tshow = T.pack . show
 
 renderPath :: [PathComponent] -> Text
-renderPath = mconcat . concatMap render . reverse
+renderPath = mconcat . concatMap render
   where
     render (Offset i) = ["[", tshow i, "]"]
-    render (Field f) | isIdentifier f = [".", f]
-                     | otherwise = ["[", tshow f, "]"]
-    isIdentifier _ = True
+    render (Field f) | isIdentifier (T.unpack f) = [".", f]
+                     | otherwise = ["["] ++ TL.toChunks (AT.encodeToLazyText f) ++ ["]"]
+    isIdentifier "" = False
+    isIdentifier (c : cs) = isInitial c && all isContinuation cs
+    isInitial c = isAsciiLower c || isAsciiUpper c || c == '_'
+    isContinuation c = isInitial c || isDigit c
 
 valueAsText :: Value -> Text
 valueAsText = TL.toStrict . AT.encodeToLazyText
