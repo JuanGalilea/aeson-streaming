1c76685c2d65a08217ce231240920a5f631699ae
Robert J. Macomber <robertm@dilithium>
2018-07-28 18:34:10 -0700

    Add conduit helpers to produce the elements of compounds

--- aeson-streaming-conduit/src/Data/Aeson/Streaming/Conduit.hs
+++ aeson-streaming-conduit/src/Data/Aeson/Streaming/Conduit.hs
@@ -1,15 +1,19 @@
 {-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE DataKinds #-}
 
 module Data.Aeson.Streaming.Conduit (
   sinkParser
 , sinkParser'
 , ParseError(..)
+, yieldElements
+, yieldEntries
 ) where
 
 import Conduit
 import Data.ByteString (ByteString)
 import qualified Data.ByteString as BS
 import Control.Monad
+import Data.Aeson (FromJSON)
 import Data.Attoparsec.ByteString (IResult(..))
 import Control.Exception
 import Data.Typeable
@@ -39,6 +43,36 @@ sinkParser p = go (parse p)
 sinkParser' :: (MonadThrow m) => Parser p -> ConduitT ByteString x m p
 sinkParser' p =
   sinkParser p >>= either throwM pure
+
+-- | Yields the remaining elements of the current compound (array or
+-- object) without their indexes or field names.  Returns a parser
+-- which will continue consuming input after the end of that compound.
+yieldElements :: (MonadThrow m, FromJSON v) => Parser (Element c p) -> ConduitT ByteString v m (NextParser p)
+yieldElements = loop
+  where
+    loop = step <=< sinkParser'
+    step (Element _ elementParser) =
+      do
+        (nextParser, value) <- sinkParser' (decodeValue' elementParser)
+        yield value
+        loop nextParser
+    step (End nextParser) =
+      pure nextParser
+
+-- | Yields the remaining elements of the current compound (array or
+-- object) with their indexes or field names.  Returns a parser which
+-- will continue consuming input after the end of that compound.
+yieldEntries :: (MonadThrow m, FromJSON v) => Parser (Element c p) -> ConduitT ByteString (Index c, v) m (NextParser p)
+yieldEntries = loop
+  where
+    loop = step <=< sinkParser'
+    step (Element idx elementParser) =
+      do
+        (nextParser, value) <- sinkParser' (decodeValue' elementParser)
+        yield (idx, value)
+        loop nextParser
+    step (End nextParser) =
+      pure nextParser
 
 -- | An attoparsec `Fail`ure as an exception
 data ParseError = ParseError ByteString [String] String
